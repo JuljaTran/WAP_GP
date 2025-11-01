cd38947ae3f5572bb0dfd5d44576f64d04045a61
ViktoriaDencheva <147704695+ViktoriaDencheva@users.noreply.github.com>
2025-11-01 16:01:52 +0100

    move pages to frontend folder and resolve merge conflicts

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,34 @@
 import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
-import Login from './pages/Login.jsx'
-import Register from './pages/Register.jsx'
+import AchievementsPage from "./pages/AchievementsPage";
+import AvatarPage from "./pages/AvatarPage";
+import CategoryPage from "./pages/CategoryPage";
+import FeedbackPage from "./pages/FeedbackPage";
+import HomePage from "./pages/HomePage";
+import LeaderboardPage from "./pages/LeaderboardPage";
+import LoginPage from "./pages/Login";
+import QuizPage from "./pages/QuizPage";
+import RegisterPage from "./pages/Register";
+import ResultPage from "./pages/ResultPage";
+
+
 
 function App() {
 
   return (
     <>
     <Router>
-      <Login />
-      <Register />
+      <Routes>
+        <Route path="/" element={<LoginPage />} />
+        <Route path="/register" element={<RegisterPage />} />
+        <Route path="/avatar" element={<AvatarPage />} />
+        <Route path="/home" element={<HomePage />} />
+        <Route path="/category/:category" element={<CategoryPage />} />
+        <Route path="/quiz/:category/:difficulty" element={<QuizPage />} />
+        <Route path="/feedback" element={<FeedbackPage />} />
+        <Route path="/result" element={<ResultPage />} />
+        <Route path="/achievements" element={<AchievementsPage />} />
+        <Route path="/leaderboard" element={<LeaderboardPage />} />
+      </Routes>
     </Router>
     </>
   )
--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,5 +1,4 @@
-import { useState } from 'react'
-import { Link } from 'react-router-dom'
+import { useState } from 'react';
 
 function Register() {
     const [name, setName] = useState('');
