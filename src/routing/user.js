1a620d4daed7c47de9af4552d22249023d9efea8
Julja <s2310238071@fhooe.at>
2025-12-08 12:15:47 +0100

    Add user.js file to handle points and avatar

--- src/routing/user.js
+++ src/routing/user.js
@@ -20,6 +20,20 @@ router.patch('/:userId/points', async(req, res) => {
             console.error(err);
             res.status(500).json({ error: "Internal server error"});
         }
+
+        const thresholds = [
+            { key: "rabbit", pts: 100 },
+            { key: "dog", pts: 500 },
+            { key: "lion", pts: 1000 }
+        ];
+
+        currentUser.unlocked = currentUser.unlocked || [];
+        thresholds.forEach(t => {
+            if (currentUser.totalPoints >= t.pts && !currentUser.unlocked.includes(t.key)) {
+            currentUser.unlocked.push(t.key);
+            }
+        });
+
 });
 
 //set Avatar
